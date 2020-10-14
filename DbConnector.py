168b6642e1c460803d52affeba03a49ed94dfee1
Kaja Sofie Lundgaard <kaja@Kajas-MacBook-Pro.local>
2020-10-14 13:10:40 +0200

    connect to db

--- DbConnector.py
+++ DbConnector.py
@@ -13,10 +13,10 @@ class DbConnector:
     """
 
     def __init__(self,
-                 DATABASE='DATABASE_NAME',
-                 HOST="tdt4225-xx.idi.ntnu.no",
-                 USER="TEST_USER",
-                 PASSWORD="test123"):
+                 DATABASE='assignment3',
+                 HOST="tdt4225-03.idi.ntnu.no",
+                 USER="gr3_assignment3",
+                 PASSWORD="passord"):
         uri = "mongodb://%s:%s@%s/%s" % (USER, PASSWORD, HOST, DATABASE)
         # Connect to the databases
         try:
